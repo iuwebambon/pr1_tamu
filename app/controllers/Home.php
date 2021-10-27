7450968211931ae3cc66a8f7352b23fe712a8ad7
marthin salakory <marthinsalakory11@gmail.com>
2021-10-27 22:23:47 +0900

    mengubah jumlah user pada halaman home, karena sebelumnya lupa dibuat

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -15,12 +15,15 @@ class Home extends Controller
         $ll = mysqli_num_rows($ll);
         $pp = mysqli_query(conn(), "SELECT * FROM tamu WHERE jenis_kelamin = 'Perempuan'");
         $pp = mysqli_num_rows($pp);
+        $us = mysqli_query(conn(), "SELECT * FROM users");
+        $us = mysqli_num_rows($us);
 
         $data = [
             'title' => 'Dashboard Tamu',
             'll'    => $ll,
             'pp'    => $pp,
-            'al'    => $al
+            'al'    => $al,
+            'us'    => $us
         ];
         $this->view('templates/header', $data);
         $this->view('index', $data);
--- app/views/index.php
+++ app/views/index.php
@@ -86,7 +86,7 @@
                                 <div class="card-block">
                                     <div class="row align-items-center">
                                         <div class="col-8">
-                                            <h4 class="text-c-blue f-w-600">500</h4>
+                                            <h4 class="text-c-blue f-w-600"><?= $us; ?></h4>
                                             <h6 class="text-muted m-b-0">Akun pengguna</h6>
                                         </div>
                                         <div class="col-4 text-right">
