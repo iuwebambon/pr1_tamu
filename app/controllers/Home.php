e778288434848bb75074df689befb42ea860ed6a
marthin salakory <marthinsalakory11@gmail.com>
2021-10-27 18:14:42 +0900

    memperbaiki halaman dashboard

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -8,8 +8,19 @@ class Home extends Controller
     }
     public function index()
     {
+
+        $al = mysqli_query(conn(), "SELECT * FROM tamu");
+        $al = mysqli_num_rows($al);
+        $ll = mysqli_query(conn(), "SELECT * FROM tamu WHERE jenis_kelamin = 'Laki-laki'");
+        $ll = mysqli_num_rows($ll);
+        $pp = mysqli_query(conn(), "SELECT * FROM tamu WHERE jenis_kelamin = 'Perempuan'");
+        $pp = mysqli_num_rows($pp);
+
         $data = [
-            'title' => 'Tamu',
+            'title' => 'Dashboard Tamu',
+            'll'    => $ll,
+            'pp'    => $pp,
+            'al'    => $al
         ];
         $this->view('templates/header', $data);
         $this->view('index', $data);
